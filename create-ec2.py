65748962516b91f9e90a1e303c9a6df83b7b2c0c
vinodh kumar <vinodh141@gmail.com>
2019-09-28 16:21:26 +0000

    little modification

--- create-ec2.py
+++ create-ec2.py
@@ -2,10 +2,12 @@ import boto3
 ec2 = boto3.resource('ec2')
 
 # create a new EC2 instance
-instances = ec2.create_instances(
+instance = ec2.create_instances(
      ImageId='ami-02f706d959cedf892',
      MinCount=1,
      MaxCount=4,
      InstanceType='t2.medium',
+	 SecurityGroups=["mysecure"],
      KeyName='vinod-keypair'
  )
+print instance[0].id
